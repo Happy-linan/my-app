c9cf0e5626ad642cc07874d7ba64816011ecd148
TENCENT\v_lnolnli <v_lnolnli@tecent.com>
2022-03-29 23:41:41 +0800

    配置路由

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    globals: {},
+    parser: "babel-eslint",
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: "module",
+    },
+    ignorePatterns: ["node_modules/"],
+    rules: {},
+    settings: {
+    },
+  };
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,40 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    // port: 9090,
+    // open: true,
+    // host: 'localhost'
+  },
+  css: {
+    loaderOptions: {
+      css: {
+        // 这里的选项会传递给 css-loader
+      },
+      postcss: {
+        // 这里的选项会传递给 postcss-loader
+        postcssOptions: {
+          plugins: [
+            require('postcss-pxtorem')({
+              rootValue: 36
+            })
+          ]
+        }
+      }
+    }
+  },
+  lintOnSave: false
+}
+
+// module.exports = {
+//   css: {
+//     loaderOptions: {
+//       css: {},
+//       postcss: {
+//         plugins: [
+//           require('postcss-px2rem')({
+//             remUnit: 37.5
+//           })
+//         ]
+//       }
+//     }
+//   }
+// }
